f5255d592dfdd2d0a6b8c94022cb54e5f7ab2d1c
Hunter Matthews <sirhunty@gmail.com>
2020-04-15 15:45:04 -0700

    ran into syntax error on api.js file; adjusted these

--- routes/api.js
+++ routes/api.js
@@ -1,30 +1,31 @@
 // requiring pages and libraries
 const router = require("express").Router();
-const storage = require("../db/storage").Router();
+const storage = require("../db/storage")
 
 //GET line; responds with notes from database
-router.get("/notes", function(req, res) {
-storage
-    .getNotes()
-    .then(notes => res.json(notes))
-    .catch(error +> res.status(500).json(error));
+router.get("/notes", function (req, res) {
+    storage
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(error => res.status(500).json(error));
 });
 
 //posting a new note or error out 
 router.post("/notes", (req, res) => {
-    .addNote(req.body)
-    .then((note) => res.json(note))
-    .catch(err => res.status(500).json(err));
+    storage
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch(error => res.status(500).json(error));
 });
 
 // Delete function/route, deletes note with id that equals to req.params.id
-router.delete("/notes/:id", function(req, res) {
-    store
-      .removeNote(req.params.id)
-      .then(() => res.json({ ok: true }))
-      .catch(err => res.status(500).json(err));
-  });
-  
-  //export module for router
-  module.exports = router;
+router.delete("/notes/:id", function (req, res) {
+    storage
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch(error => res.status(500).json(error));
+});
+
+//export module for router
+module.exports = router;
 
